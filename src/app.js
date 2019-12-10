1d39e47f07fa5c9537b5bbbd7a1f5989eea7c210
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-12-10 17:14:46 +0100

    Faffing about with bulletproof node

--- src/app.js
+++ src/app.js
@@ -1,13 +1,39 @@
-require('dotenv').config()
+import config from './config';
 
-const express = require('express')
-const server = express()
-const port = process.env.PORT || 9000
+import express from 'express';
+
+async function startServer() {
+    const app = express();
+
+    /**
+     * A little hack here
+     * Import/Export can only be used in 'top-level code'
+     * Well, at least in node 10 without babel and at the time of writing
+     * So we are using good old require.
+     **/
+    await require('./loaders').default({ expressApp: app });
+
+    app.listen(config.port, err => {
+        if (err) {
+            Logger.error(err);
+            process.exit(1);
+            return;
+        }
+        Logger.info(`
+        ################################################
+        🛡️   Server running on port ${config.port}     🛡️ 
+        ################################################
+      `);
+    });
+}
+
+startServer();
+
 
 server.get('/', (req, res) => {
-    res.status(200).json({"message": "Server's alive!"})
+    res.status(200).json({ "message": "Server's alive!" })
 })
 
 server.listen(port, () => {
-    console.log(`\n--- Server running on port ${port} ---`)
+    console.log(`\n---  ---`)
 })
